c2eb70f153526cda37921a159d8e2a3ad4ac0660
junaidh1 <48263515+junaidh1@users.noreply.github.com>
2023-12-09 11:43:01 -0600

    Implemented home page.

--- src/controllers/api/products.js
+++ src/controllers/api/products.js
@@ -1,11 +1,34 @@
-// import db from '../../config/db.js';
+import db from '../../config/db.js';
+
+// const getProducts = async (req, res) => {
+//     const products = await db.collection("products").find({});
+//     res.status(200).send(products);
+//     // TODO: If there are no query parameters, return all products in the database
+//     // TODO: If there are query parameters, return all products in the database that match the query parameters (e.g. /api/products?search=cake&filter=wedding)
+// };
 
 const getProducts = async (req, res) => {
-    // TODO: If there are no query parameters, return all products in the database
-    // TODO: If there are query parameters, return all products in the database that match the query parameters (e.g. /api/products?search=cake&filter=wedding)
-};
+    try {
+        const products = await db.collection("products").find({}).toArray();
+        const types = [...new Set(products.map(product => product.type))];
+        res.status(200).json({ products, types });
+    } catch (error) {
+      console.error("Error fetching products:", error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  };
 
-const getProduct = async (req, res) => {};
+const getProduct = async (req, res) => {
+    try {
+        const product = await db
+            .collection("products")
+            .findOne({ _id: req.params.id });
+        res.status(200).send(product);
+    } catch (error) {
+        console.error("Error fetching product:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+};
 
 const createProduct = async (req, res) => {};
 
--- src/controllers/client.js
+++ src/controllers/client.js
@@ -1,16 +1,47 @@
 // import bcrypt from "bcrypt";
 // import passport from "passport";
 
+// import getProducts from "../controllers/api/products.js";
+
 // import db from "../config/db.js";
 
-const getHomePage = (req, res) => {
-    /**
-     * TODO: if user is authenticated, render the home page with username on navbar and ability to add products to cart
-     * TODO: if user is not authenticated, render the home page with option to sign in or sign up, and no ability to add products to cart
-     */
+// const getHomePage = (req, res) => {
+//     /** await.fetch("http://localhost:3000/api/products")
+//         .then((res) => res.json())
+//         .then((data) => {
+//             console.log(data);
+//         })
+//         .catch((err) => {
+//             console.log(err);
+//         });
+//     **/
+//     /**
+//      * TODO: if user is authenticated, render the home page with username on navbar and ability to add products to cart
+//      * TODO: if user is not authenticated, render the home page with option to sign in or sign up, and no ability to add products to cart
+//      */
 
-    res.render("index");
-};
+//     res.render("index");
+// };
+
+const getHomePage = async (req, res) => {
+    try {
+        const response = await fetch("http://localhost:3000/api/products");
+        if (!response.ok) {
+            console.log("Error fetching products");
+            throw new Error("Failed to fetch products");
+        }
+        const { products, types } = await response.json();
+        // Check user authentication status here...
+        // For now, assuming user is not authenticated
+        //   const isAuthenticated = false;
+  
+        res.render("index", { products, types });
+    } catch (error) {
+            console.error("Error fetching products:", error);
+            // res.render("index", { products: [], types: [], isAuthenticated: false });
+            res.render("index", { products: [], types: [] });
+    }
+  };
 
 const getSignInPage = (req, res) => {
     res.render("signin")
