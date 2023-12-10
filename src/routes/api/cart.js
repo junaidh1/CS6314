634627765f6bfc44d0b56a6e965833e817b40bc9
spenpal <spenpal2000@gmail.com>
2023-12-10 10:54:09 -0600

    add isAuth middleware to every route

--- src/routes/api/cart.js
+++ src/routes/api/cart.js
@@ -1,20 +1,21 @@
-import express from 'express';
-
 import {
     addToCart,
     checkout,
     getCart,
     removeFromCart,
-} from '../../controllers/api/cart.js';
+} from "../../controllers/api/cart.js";
+
+import express from "express";
+import { isAuth } from "../../util.js";
 
 const router = express.Router();
 
-router.post("/checkout", checkout); // User route
+router.post("/checkout", isAuth, checkout); // User route
 
-router.get("/", getCart); // User route
+router.get("/", isAuth, getCart); // User route
 
-router.post("/:id", addToCart); // User route
+router.post("/:id", isAuth, addToCart); // User route
 
-router.delete("/:id", removeFromCart); // User route
+router.delete("/:id", isAuth, removeFromCart); // User route
 
 export default router;
