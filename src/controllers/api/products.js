2ed1253222cb2615ee1a309aaa1ac0f240cf6654
Maryam Hashmi <maryam.hashmi@gmail.com>
2023-12-09 20:45:55 -0600

    Product details page related changes.

--- src/controllers/api/products.js
+++ src/controllers/api/products.js
@@ -18,17 +18,37 @@ const getProducts = async (req, res) => {
     }
 };
 
+// const getProduct = async (req, res) => {
+//     try {
+//         const product = await db
+//             .collection("products")
+//             .findOne({ _id: req.params.id });
+//         res.status(200).send(product);
+//     } catch (error) {
+//         console.error("Error fetching product:", error);
+//         res.status(500).json({ error: "Internal Server Error" });
+//     }
+// };
 const getProduct = async (req, res) => {
     try {
-        const product = await db
-            .collection("products")
-            .findOne({ _id: req.params.id });
-        res.status(200).send(product);
+      // Log the productId to ensure it is correct
+      console.log('Backend Received Product ID:', req.params.id);
+  
+        const product = await db.collection('products').findOne({ id: req.params.id});
+        const products = await db.collection('products').find({}).toArray();
+        const types = [...new Set(products.map(product => product.type))];
+      if (!product) {
+        return res.status(404).json({ error: 'Product not found' });
+      }
+  
+      res.status(200).json({product,types});
     } catch (error) {
-        console.error("Error fetching product:", error);
-        res.status(500).json({ error: "Internal Server Error" });
+      console.error('Error fetching product:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
     }
-};
+  };
+  
+  
 
 const createProduct = async (req, res) => {};
 
