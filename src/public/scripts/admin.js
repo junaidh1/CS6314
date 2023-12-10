5d8d9638f4e1a50e6d13d3f189ddd07bdeeb5bbc
spenpal <spenpal2000@gmail.com>
2023-12-10 16:15:18 -0600

    add admin code

---
+++ src/public/scripts/admin.js
@@ -0,0 +1,14 @@
+function editProduct(productId) {
+    // Add logic to handle editing the product with the given ID
+    console.log(`Editing product with ID: ${productId}`);
+}
+
+function deleteProduct(productId) {
+    // Add logic to handle deleting the product with the given ID
+    console.log(`Deleting product with ID: ${productId}`);
+}
+
+function addNewProduct() {
+    // Add logic to handle adding a new product
+    console.log("Adding a new product");
+}
