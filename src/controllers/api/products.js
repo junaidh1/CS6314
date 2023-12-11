7d6ac13edb48d840dc30a6e676dd9420d7fac7b1
spenpal <spenpal2000@gmail.com>
2023-12-10 23:03:13 -0600

    formatting fix

--- src/controllers/api/products.js
+++ src/controllers/api/products.js
@@ -1,51 +1,53 @@
-import db from "../../config/db.js";
-
+import db from '../../config/db.js';
 
 const getProducts = async (req, res) => {
-  try {
-      const { search, filter } = req.query;
-      let productsQuery = {};
-      let products = await db.collection("products").find().toArray();
-      let types = [...new Set(products.map(product => product.type))];
+    try {
+        const { search, filter } = req.query;
+        let productsQuery = {};
+        let products = await db.collection("products").find().toArray();
+        let types = [...new Set(products.map((product) => product.type))];
 
-      if (search) {
-          productsQuery = {
-              $or: [
-                  { name: { $regex: new RegExp(search, "i") } },
-                  { description: { $regex: new RegExp(search, "i") } }
-              ]
-          };
-      } else if (filter) {
-          productsQuery = { type: { $regex: new RegExp(filter, "i") } };
-      }
+        if (search) {
+            productsQuery = {
+                $or: [
+                    { name: { $regex: new RegExp(search, "i") } },
+                    { description: { $regex: new RegExp(search, "i") } },
+                ],
+            };
+        } else if (filter) {
+            productsQuery = { type: { $regex: new RegExp(filter, "i") } };
+        }
 
-      if (search || filter) {
-          products = await db.collection("products").find(productsQuery).toArray();
-      }
-      res.status(200).json({ products, types });
-  } catch (error) {
-      console.error("Error fetching products:", error);
-      res.status(500).json({ error: "Internal Server Error" });
-  }
+        if (search || filter) {
+            products = await db
+                .collection("products")
+                .find(productsQuery)
+                .toArray();
+        }
+        res.status(200).json({ products, types });
+    } catch (error) {
+        console.error("Error fetching products:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
 };
 
 const getProduct = async (req, res) => {
-    try {  
-        const product = await db.collection('products').findOne({ id: req.params.id});
-        const products = await db.collection('products').find({}).toArray();
-        const types = [...new Set(products.map(product => product.type))];
-      if (!product) {
-        return res.status(404).json({ error: 'Product not found' });
-      }
-  
-      res.status(200).json({product,types});
+    try {
+        const product = await db
+            .collection("products")
+            .findOne({ id: req.params.id });
+        const products = await db.collection("products").find({}).toArray();
+        const types = [...new Set(products.map((product) => product.type))];
+        if (!product) {
+            return res.status(404).json({ error: "Product not found" });
+        }
+
+        res.status(200).json({ product, types });
     } catch (error) {
-      console.error('Error fetching product:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
+        console.error("Error fetching product:", error);
+        res.status(500).json({ error: "Internal Server Error" });
     }
-  };
-  
-  
+};
 
 const createProduct = async (req, res) => {};
 
